e9aa52d76fdfc6ed30d2f909b305f5f68fc3f6de
MultiVenza LeadHunter <multivenza@leadhunter.local>
2026-04-30 02:57:41 -0500

    Reset config for clean deploy

--- next.config.js
+++ next.config.js
@@ -1,33 +1,7 @@
-const withPWA = require('@ducanh2912/next-pwa').default({
-  dest: 'public',
-  cacheOnFrontEndNav: false,
-  aggressiveFrontEndNavCaching: false,
-  reloadOnOnline: true,
-  disable: process.env.NODE_ENV === 'development',
-  workboxOptions: {
-    disableDevLogs: true,
-    runtimeCaching: [
-      {
-        urlPattern: /^https:\/\/.*\.supabase\.co\/rest\/.*/i,
-        handler: 'NetworkFirst',
-        options: {
-          cacheName: 'supabase-rest',
-          expiration: { maxEntries: 64, maxAgeSeconds: 5 * 60 },
-          networkTimeoutSeconds: 10,
-        },
-      },
-      {
-        urlPattern: /^https:\/\/.*\.supabase\.co\/auth\/.*/i,
-        handler: 'NetworkOnly',
-      },
-    ],
-  },
-})
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   images: { remotePatterns: [] },
 }
 
-module.exports = withPWA(nextConfig)
+module.exports = nextConfig
